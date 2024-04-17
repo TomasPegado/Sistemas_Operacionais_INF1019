741a4a08105a3cfb7e384fb57fcd1aa09c2c0613
TomasPegado <lenzitomas00@gmail.com>
2024-04-17 12:34:43 -0300

    Lab3

--- Labs/Lab3/ex4.c
+++ Labs/Lab3/ex4.c
@@ -1,53 +1,81 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 
+// Função para tratar sinal SIGCONT
+void sigcont_handler(int sig)
+{
+    printf("Processo %d continuado com SIGCONT\n", getpid());
+}
 
-#define EVER ;;
-
-int status;
-int shmid;
-
-//  Ponteiros pra guardar PID dos filhos
+int main(void)
+{
+    int contador = 0, alternador = 0;
+    int shmid;
     int *p1, *p2;
 
-// Criar memória compartilhada
-shmid = shmget(IPC_PRIVATE, sizeof(int) * 2, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    // Criar memória compartilhada
+    shmid = shmget(IPC_PRIVATE, sizeof(int) * 2, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    p1 = (int *)shmat(shmid, NULL, 0);
+    p2 = p1 + 1;
 
-p1 = (int *)shmat(shmid, NULL, 0);
-p2 = p1 + 1;
-int main(void){
-
-
-    for (int i = 0; i<2; i++){
-
-        if (fork() != 0){
-
-            printf("Processo %d iniciado.\n",i+1);
-            
-        } else {
+    for (int i = 0; i < 2; i++)
+    {
+        int pid = fork();
+        if (pid > 0)
+        {
+            // Processo pai
+            printf("Processo %d iniciado.\n", i + 1);
+            if (i == 0)
+                *p1 = pid;
+            else
+                *p2 = pid;
+        }
+        else if (pid == 0)
+        {
+            // Processo filho
             printf("PID do processo: %d.\n", getpid());
-            if (i == 0){
-                *p1 = getpid();
-            } else {
-                *p2 = getpid();
-            }
-            for (EVER);
+
+            // Registrar o tratador para SIGCONT
+            signal(SIGCONT, sigcont_handler);
+
+            while (1)
+                ; // Loop infinito
+        }
+        else
+        {
+            // Erro ao criar processo filho
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
     }
 
-printf("PID do processo 1: %d.\n", *p1);
-printf("PID do processo 2: %d.\n", *p2);
-while (waitpid(-1,&status,0)!=-1);
+    // Dar tempo para que os processos filhos configurem os tratadores de sinal
+    sleep(1);
 
+    // Alternar entre os processos filhos usando SIGSTOP e SIGCONT
+    while (contador < 10)
+    {
+        int pidToStop = alternador == 0 ? *p1 : *p2;
+        int pidToCont = alternador == 0 ? *p2 : *p1;
 
+        kill(pidToStop, SIGSTOP);
+        kill(pidToCont, SIGCONT);
 
+        printf("Processo %d pausado, processo %d continuado.\n", pidToStop, pidToCont);
+
+        alternador = 1 - alternador;
+        contador++;
+        sleep(1); // Dar um tempo entre as alternâncias
+    }
+
+    // Terminar os processos filhos depois de 10 alternâncias
+    kill(*p1, SIGKILL);
+    kill(*p2, SIGKILL);
+
+    return 0;
 }
-
-// void changeHandles(int sinal)
