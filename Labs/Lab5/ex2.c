4a305d804dc1f085be4e978af016e231cd8bca01
TomasPegado <lenzitomas00@gmail.com>
2024-06-04 20:09:20 -0300

    lab6 ex2

--- Labs/Lab5/ex2.c
+++ Labs/Lab5/ex2.c
@@ -4,40 +4,59 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
 #define NUM_FILHOS 2
 
 int main(void)
 {
 
-    int filho;
+    int status;
     int fwFifo;
     int frFifo;
-    if (mkfifo("segundaFifo", S_IRUSR | S_IWUSR) == 0)
+    char ch;
+    char mensagem[] = "escrita ";
+    if (access("segundaFifo", F_OK) == -1)
     {
-        puts("FIFO criada com sucesso");
-        return 0;
+        if (mkfifo("segundaFifo", S_IRUSR | S_IWUSR) != 0)
+        {
+            fprintf(stderr, "Erro ao criar FIFO %s\n", "segundaFifo");
+            return -1;
+        }
     }
-    puts("Erro na criação da FIFO");
-    return -1;
 
-    frFifo = open("segundaFifo", O_RDONLY | O_NONBLOCK);
     for (int i = 0; i < NUM_FILHOS; i++)
     {
 
         if (fork() != 0)
         {
 
-            puts("Abrindo FIFO para escrita no filho %d", getpid);
+            waitpid(-1, &status, 0);
+            printf("Abrindo FIFO para escrita no filho %d\n", getpid());
             if ((fwFifo = open("segundaFifo", O_WRONLY | O_NONBLOCK)) < 0)
             {
-                fprintf(stderr, "Erro ao abrir a FIFO %s\n", FIFO);
+                fprintf(stderr, "Erro ao abrir a FIFO %s\n", "segundaFifo");
                 return -2;
             }
             puts("Começando a escrever...");
-            write(fpFIFO, mensagem, strlen(mensagem));
+            write(fwFifo, mensagem, strlen(mensagem));
             puts("Fim da escrita");
-            close(fpFIFO);
+            close(fwFifo);
+        }
+        else
+        {
+            if ((frFifo = open("segundaFifo", O_RDONLY | O_NONBLOCK)) < 0)
+            {
+                fprintf(stderr, "Erro ao abrir a FIFO %s para leitura\n", "segundaFifo");
+                return -2;
+            };
         }
     }
+
+    waitpid(-1, &status, 0);
+    puts("Começando a ler...");
+    while (read(frFifo, &ch, sizeof(ch)) > 0)
+        putchar(ch);
+    puts("Fim da leitura");
+    close(frFifo);
 }
