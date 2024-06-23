cb96d15a0a56eb9d4d526344f61783ca627510f0
TomasPegado <lenzitomas00@gmail.com>
2024-06-23 16:34:31 -0300

    iteração e structs

--- Trabalho2/simulador.c
+++ Trabalho2/simulador.c
@@ -8,6 +8,53 @@
 
 #define NUM_PROCESSOS 3
 #define TEMPO_EXECUCAO 3
+#define NUM_PAGINAS 16
+#define NUM_QUADROS 16
+
+typedef struct
+{
+    int quadro;
+    int presente;
+    int referenciado;
+    int modificado;
+    int contador;
+} EntradaTabelaPagina;
+
+typedef struct
+{
+    EntradaTabelaPagina paginas[NUM_PAGINAS];
+} TabelaPagina;
+
+typedef struct
+{
+    int quadro[NUM_QUADROS];
+} MemoriaPrincipal;
+
+MemoriaPrincipal memoria;
+TabelaPagina tabelas[NUM_PROCESSOS];
+
+void inicializar_memoria()
+{
+    for (int i = 0; i < NUM_QUADROS; i++)
+    {
+        memoria.quadro[i] = -1; // -1 indica que o quadro está livre
+    }
+}
+
+void inicializar_tabelas()
+{
+    for (int p = 0; p < NUM_PROCESSOS; p++)
+    {
+        for (int i = 0; i < NUM_PAGINAS; i++)
+        {
+            tabelas[p].paginas[i].quadro = -1;
+            tabelas[p].paginas[i].presente = 0;
+            tabelas[p].paginas[i].referenciado = 0;
+            tabelas[p].paginas[i].modificado = 0;
+            tabelas[p].paginas[i].contador = 0;
+        }
+    }
+}
 
 void gerenciador(const char *mensagem)
 {
@@ -35,18 +82,64 @@ void executar_processo(const char *nome_arquivo)
     exit(EXIT_SUCCESS);
 }
 
+void iteracao(int *politica, int *num_rodadas, int *parametroWS)
+{
+
+    printf("Selecione a política de substituição de páginas:\n");
+    printf("1. Not Recently Used (NRU)\n");
+    printf("2. Segunda Chance\n");
+    printf("3. LRU/Aging\n");
+    printf("4. Working Set\n");
+    printf("Digite o número da política: ");
+    scanf("%d", politica);
+
+    printf("Digite o número de rodadas: ");
+    scanf("%d", num_rodadas);
+
+    if (*politica == 1)
+    {
+        printf("Not Recently Used (NRU)\n");
+        printf("Numero de Rodadas: %d\n", *num_rodadas);
+    }
+    if (*politica == 2)
+    {
+        printf("Segunda Chance\n");
+        printf("Numero de Rodadas: %d\n", *num_rodadas);
+    }
+    if (*politica == 3)
+    {
+        printf("LRU/Aging\n");
+        printf("\nNumero de Rodadas: %d\n", *num_rodadas);
+    }
+    if (*politica == 4)
+    {
+        printf("Informe o parametro: ");
+        scanf("%d", parametroWS);
+        printf("Working Set (%d)\n", *parametroWS);
+        printf("Numero de Rodadas: %d\n", *num_rodadas);
+    }
+}
+
 int main()
 {
+
+    int politica;
+    int num_rodadas;
+    int parametroWS;
+
     pid_t pids[NUM_PROCESSOS];
     const char *arquivos[NUM_PROCESSOS] = {"acessos_P1.txt", "acessos_P2.txt", "acessos_P3.txt"};
 
+    inicializar_memoria();
+    inicializar_tabelas();
+
+    iteracao(&politica, &num_rodadas, &parametroWS);
     // Cria os processos filhos
     for (int i = 0; i < NUM_PROCESSOS; i++)
     {
         if ((pids[i] = fork()) == 0)
         {
             // Código do processo filho
-            // kill(pids[i], SIGSTOP);
             executar_processo(arquivos[i]);
         }
         else if (pids[i] < 0)
